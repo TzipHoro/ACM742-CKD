e7ee42f7737557bf10d2088053e4219046906d0b
bhealy5 <167721859+bhealy5@users.noreply.github.com>
2024-04-28 19:20:36 +0100

    Add files via upload

---
+++ File/ckd_KNN.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 28 18:00:52 2024
+
+@author: bheal
+"""
+
+from sklearn.neighbors import KNeighborsClassifier
+import pandas as pd
+import numpy as np
+
+#set-up data
+Train = pd.read_csv("C:\\Users\\bheal\\anaconda3\\envs\\myConda\\Data\\ckd_training.csv",header=0)
+Test = pd.read_csv("C:\\Users\\bheal\\anaconda3\\envs\\myConda\\Data\\ckd_test.csv",header=0)
+Train = Train.to_numpy()
+Test = Test.to_numpy()
+X_train = Train[:, 0:24]
+y_train = Train [:, 24:25]
+X_test = Test[:, 0:24]
+y_test = Test[:, 24:25]
+
+#train the model
+neighbor = KNeighborsClassifier(n_neighbors=5)
+neighbor.fit(X_train,y_train)
+
+#Predict
+outcome = neighbor.predict(X_test)
